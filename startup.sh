3a172d218df9e6f00eca38c8dd89d19b580bf24f
Dariusz Jakubowski <celber@me.com>
2017-07-13 15:52:06 +0200

    #146@onboarding exported global NODE_PATH

--- startup.sh
+++ startup.sh
@@ -5,4 +5,5 @@ echo Waiting for Selenium Server
 /opt/bin/entry_point.sh 2>&1 > ./result/selenium_grid.log &
 sleep 5
 echo Starting WebDriver
+export NODE_PATH='$(NODE_PATH):/home/seluser/node_modules/'
 npm start
