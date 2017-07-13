64775e295969125bea1eb0913a96a2acf3b5a92d
Dariusz Jakubowski <celber@me.com>
2017-07-13 14:45:10 +0200

    #146@onboarding changed result path for startup script

--- startup.sh
+++ startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo Waiting for Selenium Server
-/opt/bin/entry_point.sh 2>&1 > ./result/selenium_grid.log &
+/opt/bin/entry_point.sh 2>&1 > /result/selenium_grid.log &
 sleep 5
 echo Starting WebDriver
 npm start
